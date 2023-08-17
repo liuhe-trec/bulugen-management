632c5c4ead19c317031bceb538e83add36f1179e
LiuHe <wangziyejimo@sina.com>
2023-08-18 00:00:06 +0800

    Modify git don't ignorecase

---
+++ src/views/Index/Index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="index-w">
+        <div class="title">Index.vue</div>
+        <router-view />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.index-w{
+    background-color: #ff0;
+    .title{
+        margin: 30px;
+    }
+}
+</style>
