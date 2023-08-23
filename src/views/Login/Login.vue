5bd61bbeac272c8b961734faddb59b33091344b5
LiuHe <wangziyejimo@sina.com>
2023-08-23 23:45:15 +0800

    Add background image

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -7,7 +7,20 @@
 </template>
 
 <style lang="scss" scoped>
+.content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url("../../assets/images/login_bg.jpg");
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .login-content {
+  text-align: center;
   @include bg();
   width: 640px;
   height: 840px;
